2c197946ad1e55b72edea3762f910ba496a38765
wil.i.am <wiliam.tae@gmail.com>
2020-03-28 10:58:16 -0300

    project init

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import Acceleration from './src/screens/Acceleration.js';
+
+const App = () => <Acceleration/>
+
+export default App;
