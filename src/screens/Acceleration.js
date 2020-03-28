280c9a168e90c171757ac24e505f11bb9f61ae2f
wil.i.am <wiliam.tae@gmail.com>
2020-03-28 13:14:03 -0300

    challenge error with image on run test

--- src/screens/Acceleration.js
+++ src/screens/Acceleration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, ScrollView } from 'react-native';
 
 const accelerations = [{
   "slug": "reactnative-online-1",
@@ -81,29 +81,108 @@ const accelerations = [{
   "company_count": 1
 }]
 
+function Item({ title, image, course, date }) {
+  const courseImg = {uri: 'https://fakeimg.pl/150x150/'}
+
+  return (
+    <View style={styles.courses}>
+      <View style={styles.courseImgCont}>
+        <Image
+          style={styles.courseImg}
+          source={courseImg}
+        />
+      </View>
+      <View style={styles.courseInfos}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.location}>{course}</Text>
+        <Text style={styles.date}>{date.split('T')[0].split('-').reverse().join('/')}</Text>
+      </View>
+    </View>
+  );
+}
+
+const codenationLogo = {uri: "https://forum.codenation.com.br/uploads/default/original/2X/2/2d2d2a9469f0171e7df2c4ee97f70c555e431e76.png"}
+
 const Acceleration = () => {
   return (
     <View>
-      <Text>Texto Codenation Logo</Text>
-      <Text>Linha inferior</Text>
-        {
-          accelerations.map((elem, ind) => {
-            return(
-              <View key={ind}>
-                <Image source={{uri:`https://www.fillmurray.com/300/300`}}/>
-                <View>
-                  <Text>{elem.name}</Text>
-                  <Text>{elem.location}</Text>
-                  <Text>{elem.start_at}</Text>
-                </View>
-              </View>
-            );
-          })
-        }
+        <Image style={styles.logo}
+          source={codenationLogo}
+        />
+        <Text style={styles.line}></Text>
+        <ScrollView style={styles.container}>
+          <FlatList
+            data={accelerations}
+            renderItem={({ item }) => {
+              return(
+                <Item
+                  date={item.start_at}
+                  course={item.location}
+                  title={item.name}
+                />
+               )}}
+            keyExtractor={item => item.slug}
+          />
+        </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    marginBottom: 30
+  },
+  line: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    height: 4,
+    backgroundColor: '#7800FF'
+
+  },
+  logo: {
+    marginLeft: 30,
+    marginTop: 50,
+    marginBottom: 20,
+    justifyContent: 'flex-start',
+    width: 250,
+    height: 50
+  },
+  courses: {
+    borderWidth: 2,
+    borderColor: '#EFEFEF',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 20,
+    marginLeft: 30,
+    marginRight: 30
+  },
+  courseInfos: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+  courseImg: {
+    borderWidth: 2,
+    borderColor: '#EFEFEF',
+    width: 100,
+    height: 100
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  location: {
+    color: '#7800FF',
+    fontSize: 18
+  },
+  date: {
+    fontSize: 18
+  }
+});
 
 export default Acceleration;
